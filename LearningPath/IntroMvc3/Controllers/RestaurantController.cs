8ebc0d7a371df10fe2b3332314f8168c4284ab90
swmiller <Wright5$$5-z>
2015-05-21 17:09:51 -0500

    Module 3 changes.

---
+++ LearningPath/IntroMvc3/Controllers/RestaurantController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace IntroMvc3.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        OdeToFoodDB _db = new OdeToFoodDB();
+
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            var model = _db.Restaurants;
+
+            return View(model);
+        }
+    }
+}
