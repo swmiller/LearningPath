086026ff3c031eee7cb28d85f2849285d1be3021
swmiller <Wright5$$5-z>
2015-05-28 12:49:16 -0500

    Moduel 4 changes.

--- LearningPath/IntroMvc3/Controllers/RestaurantController.cs
+++ LearningPath/IntroMvc3/Controllers/RestaurantController.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using IntroMvc3.Models;
 using OdeToFood.Models;
 
 namespace IntroMvc3.Controllers
 {
     public class RestaurantController : Controller
     {
-        OdeToFoodDB _db = new OdeToFoodDB();
+        private readonly OdeToFoodDB _db = new OdeToFoodDB();
 
         // GET: Restaurant
-        public ActionResult Index()
+        public ActionResult Index(string state)
         {
-            var model = _db.Restaurants;
+            ViewBag.States = _db.Restaurants.Select(r => r.Address.State).Distinct();
+
+            IQueryable<Restaurant> model = _db.Restaurants
+                .OrderBy(r => r.Address.City)
+                .Where(r => r.Address.State == state || (state == null));
 
             return View(model);
         }
--- LearningPath/IntroMvc3/Models/Restaurant.cs
+++ LearningPath/IntroMvc3/Models/Restaurant.cs
@@ -9,6 +9,7 @@ namespace IntroMvc3.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public string ChefsName { get; set; }
         public Address Address { get; set; }
     }
 }
